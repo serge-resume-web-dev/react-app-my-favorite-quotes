05dd8ca80b2a9bbb8f881b0ae79977715300ec18
Serge <protopulos.pulatikos@yandex.ru>
2019-06-15 20:24:07 +0300

    firebase initialized; project build production; production version deployed to firebase;

---
+++ build/precache-manifest.666d10a0724b1500966d144efdcbc37f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ebaa08bb2f9c19ad2b39",
+    "url": "/static/js/main.ebaa08bb.chunk.js"
+  },
+  {
+    "revision": "393ee9f3486471ae84e9",
+    "url": "/static/js/1.393ee9f3.chunk.js"
+  },
+  {
+    "revision": "ebaa08bb2f9c19ad2b39",
+    "url": "/static/css/main.9b102390.chunk.css"
+  },
+  {
+    "revision": "ab038c6c04573c078be7013bcb9be281",
+    "url": "/index.html"
+  }
+];
